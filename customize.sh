9a7bd74bd347b1e2bebdd03034b26d636881a04e
lululoid <nreiwn@gmail.com>
2023-01-28 12:50:01 +0700

    remove non_functional function fix json file

--- customize.sh
+++ customize.sh
@@ -17,36 +17,15 @@ ui_print " █▀▀ █▀▀█ █░░░█ ░▀░ █▀▀▄ ▀▀�
 ui_print " █▀▀ █▄▄▀ █▄█▄█ ▀█▀ █░░█ ▄▀░ █▀▀ █▄▄▀ █▄▀█"
 ui_print " ▀▀▀ ▀░▀▀ ░▀░▀░ ▀▀▀ ▀░░▀ ▀▀▀ ▀▀▀ ▀░▀▀ █▄▄█"
 ui_print " ==================:)====================="; sleep 2
-
-# Checking if lmkd is costumizable or not
-lmkd_check()
-    local is_cos=False
-    local default_thrashing_limit=`grep_get_prop ro.lmk.thrashing_limit`
     
-    if [[ $(grep_get_prop ro.lmk.thrashing_limit) != 100 ]]; then
-        resetprop ro.lmk.thrashing_limit $((default_thrashing_limit + 10))
-    else
-        resetprop ro.lmk.thrashing_limit $((default_thrashing_limit - 10))
-    fi
-    if [ $default_thrashing_limit != $(grep_get_prop ro.lmk.thrashing_limit) ]; then
-        ui_print "- lmkd tweak is not supported"
-        ui_print "  some memory tweak benefit may not be achieveble."
-    fi
-    
-is_miui() {
-	is_miui_14=$(grep_get_prop ro.miui.ui.version.code)
-	ui_print $is_miui_14
-
-	if [[ ! -z $is_miui_14 && ${is_miui_14} != 14 ]]; then
-		ui_print "  MIUI memory management sucks. lmkd tweak won't work."
-		ui_print "  lmkd tweak may not applied"; sleep 2
-	elif [ ${is_miui_14} == 14 ]; then
-		ui_print "  Maybe lmkd tweak working on MIUI 14?. Tell me if it wasn't"; sleep 2
-	else
-        ui_print "- lmkd multitasking tweak applied."
-    	ui_print "  Give the better of your RAM."
-    	ui_print "  RAM better being filled than left unused"
-	fi
+lmkd_apply() {
+    for prop in $(cat $MODDIR/system.prop); do
+        resetprop $(echo $prop | sed s/=/' '/)
+    done
+    ui_print "- lmkd multitasking tweak applied."
+    ui_print "  Give the better of your RAM."
+    ui_print "  RAM better being filled with something"
+    ui_print "  useful than left unused"
 }
 
 make_swap() {
@@ -64,7 +43,7 @@ ui_print "- Checking available storage"; sleep 2
 ui_print "  $((available / 1024))MB is available"; sleep 2
 ui_print "  $((size / 1024))MB needed";sleep 2
 if [ ${available} > ${size} ]; then
-		make_swap; is_miui; lmkd_check
+		make_swap; lmkd_apply
 		ui_print "- Set up ZRAM size and SWAP size"; sleep 2
 		ui_print "  $((size / 1024))MB ZRAM + $((size / 1024))MB SWAP"; sleep 2
 		ui_print "- If this your first installation."
