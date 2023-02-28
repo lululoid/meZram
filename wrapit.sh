e7be85a02d9744da984672c7cd28f7b2b7214fe8
lululoid <erwinzer0@localhost.localdomain>
2023-02-28 21:51:19 +0700

    fix json

--- wrapit.sh
+++ wrapit.sh
@@ -3,7 +3,7 @@
 zipfiles=$(cat zip-list.txt)
 
 sed -i "s#v[0-9]\+\.[0-9]\+-beta/meZram-v[0-9]\+\.[0-9]\+_[0-9]\+-beta#v$1-beta/meZram-v$1_$2-beta#g" meZram.json
-sed -i "s#versionCode\"\: [0-9]\+#\"versionCode\"\: $2#g" meZram.json 
+sed -i "s#\"versionCode\"\: \"[0-9]\+\"#\"versionCode\"\: \"$2\"#g" meZram.json 
 sed -i "s#versionCode\=[0-9]\+#versionCode\=$2#g" module.prop
 sed -i "s#v[0-9]\+\.[0-9]\+-beta#v$1-beta#g" meZram.json 
 sed -i "s#v[0-9]\+\.[0-9]\+-beta#v$1-beta#g" module.prop 
