e53531d350901cacd939fcd50fb8953270b7c5fa
lululuid <nreiwn@gmail.com>
2023-04-06 05:34:52 +0700

    update rm_prop_reinit

--- service.sh
+++ service.sh
@@ -48,24 +48,40 @@ done &
 
 rm_prop_reinit(){                                
 	for prop in "$@"; do
-    	[ "$(getprop "$prop")" ] && resetprop --delete "$prop" && resetprop lmkd.reinit 1 && echo "- $* deleted" 1>> "$MODDIR"/meZram.log
+    	[ "$(resetprop "$prop")" ] && resetprop --delete "$prop" && resetprop lmkd.reinit 1 && echo "- $* deleted" 1>> "$MODDIR"/meZram.log
 	done
 }
 
-while true; do
-    tlc="persist.device_config.lmkd_native.thrashing_limit_critical"
-    err="persist.device_config.lmkd_native.thrashing_limit_"
-    # minfree="sys.lmk.minfree_levels"
-	tl="ro.lmk.thrashing_limit"
+if [ ! -d /data/adb/meZram ]; then
+	logger "- Trying to make folder"
+	mkdir -p /data/adb/meZram 2>> "$MODDIR"/meZram.log
+fi
 
+while true; do
+	sleep 1m 
+	cp "$MODDIR"/*.log /data/adb/meZram 
+done &
+
+
+tlc="persist.device_config.lmkd_native.thrashing_limit_critical"
+err="persist.device_config.lmkd_native.thrashing_limit_"
+# minfree="sys.lmk.minfree_levels"
+tl="ro.lmk.thrashing_limit"
+
+while true; do
 	# prop that need to be removed
-	set -- "ro.lmk.low" "ro.lmk.medium" "ro.lmk.critical" "ro.lmk.critical_upgrade" "ro.lmk.upgrade_pressure" "ro.lmk.downgrade_pressure" "ro.lmk.kill_heaviest_task" "ro.lmk.kill_timeout_ms" "ro.lmk.psi_complete_stall_ms" "ro.lmk.thrashing_limit_decay" "mezram_test"
+	run=true
+	while $run ; do
+		set -- "ro.lmk.low" "ro.lmk.medium" "ro.lmk.critical" "ro.lmk.critical_upgrade" "ro.lmk.upgrade_pressure" "ro.lmk.downgrade_pressure" "ro.lmk.kill_heaviest_task" "ro.lmk.kill_timeout_ms" "ro.lmk.psi_complete_stall_ms" "ro.lmk.thrashing_limit_decay" "mezram_test"
+		run=false
+	done
+
 	for prop in "$@"; do
 		rm_prop_reinit "$prop" 1>> "$MODDIR"/meZram.log
 	done
 
     rm_prop_reinit $tlc $err $tl 1>> "$MODDIR"/meZram.log
-	if [ "$(getprop ro.miui.ui.version.code)" -eq 13 ]; then
+	if [ "$(resetprop ro.miui.ui.version.code)" -eq 13 ]; then
 		rm_prop_reinit $tl 1>> "$MODDIR"/meZram.log
 	fi
-done
+done &
