0d94141776cec86c261f87d8179bded5374cbbf0
lululuid <nreiwn@gmail.com>
2023-04-09 11:20:10 +0700

    merge with emit_enola

--- service.sh
+++ service.sh
@@ -8,7 +8,7 @@ zram_size=$(((totalmem / 2) * 1024))
 lmkd_pid=$(getprop init.svc_debug_pid.lmkd)
 
 logger(){
-    false && echo "$*" >> "$MODDIR"/meZram.log
+    true && echo "$*" >> "$MODDIR"/meZram.log
 }
 
 logger "zram_size = $zram_size"
@@ -18,41 +18,61 @@ logger "lmkd_pid = $lmkd_pid"
 
 for zram0 in /dev/block/zram0 /dev/zram0; do
     if [ -n "$(ls $zram0)" ]; then
-	swapoff $zram0 2> "$MODDIR"/meZram.log
-	echo 1 > /sys/block/zram0/reset 2>> "$MODDIR"/meZram.log 
-	# Set up zram size, then turn on both zram and swap
-	echo $zram_size > /sys/block/zram0/disksize 2>> "$MODDIR""/meZram."log 
-	# Set up maxium cpu streams
-	echo "$NRDEVICES" > /sys/block/zram0/max_comp_streams 2>> "$MODDIR"/meZram.log 
-	mkswap $zram0 2>> "$MODDIR"/meZram.log 
-	swapon $zram0 2>> "$MODDIR"/meZram.log 
-	echo "$zram0 succesfully activated" > "$MODDIR"/meZram.log 
+		swapoff $zram0 2> "$MODDIR"/meZram.log
+		echo 1 > /sys/block/zram0/reset 2>> "$MODDIR"/meZram.log 
+
+		# Set up zram size, then turn on both zram and swap
+		echo $zram_size > /sys/block/zram0/disksize 2>> "$MODDIR""/meZram."log 
+
+		# Set up maxium cpu streams
+		echo "$NRDEVICES" > /sys/block/zram0/max_comp_streams 2>> "$MODDIR"/meZram.log 
+		mkswap $zram0 2>> "$MODDIR"/meZram.log 
+		swapon $zram0 2>> "$MODDIR"/meZram.log 
+		echo "$zram0 succesfully activated" > "$MODDIR"/meZram.log 
     else
-	echo "$zram0 not exist in this device" >> "$MODDIR"/meZram.log 
+		echo "$zram0 not exist in this device" >> "$MODDIR"/meZram.log 
     fi
 done
 
-swapon /data/swap_file 2>> "$MODDIR"/meZram.log 
+swapon /data/swap_file >> "$MODDIR"/meZram.log 
 
-echo '1' > /sys/kernel/tracing/events/psi/enable 2>> "$MODDIR"/meZram.log 
-resetprop lmkd.reinit 1
+# echo '1' > /sys/kernel/tracing/events/psi/enable 2>> "$MODDIR"/meZram.log 
 
 # peaceful logger
 while true; do
-    logcat -G 10M
-    logcat --pid "$lmkd_pid" -t 100 -f "$MODDIR"/lmkd.log &
-    sleep 5m
+    logcat --pid "$lmkd_pid" -t 1000 -f "$MODDIR"/lmkd.log
+    sleep 1m
 done &
 
-rm_prop_reinit(){
-    for prop in "$@"; do
-        [ "$(resetprop "$prop")" ] && resetprop --delete "$prop" && resetprop lmkd.reinit 1
-    done
+rm_prop(){                                
+	for prop in "$@"; do
+		[ "$(resetprop "$prop")" ] && resetprop --delete "$prop" && logger "$prop deleted" >> "$MODDIR"/meZram.log
+	done
 }
 
+if [ ! -d /data/adb/meZram ]; then
+	logger "- Trying to make folder"
+	mkdir -p /data/adb/meZram 2>> "$MODDIR"/meZram.log
+fi
+
 while true; do
-    tlc="persist.device_config.lmkd_native.thrashing_limit_critical"
-    err="persist.device_config.lmkd_native.thrashing_limit_"
-    # minfree="sys.lmk.minfree_levels"
-    rm_prop_reinit $tlc $err 2>> "$MODDIR"/meZram.log 
-done
+	sleep 1m 
+	cp "$MODDIR"/*.log /data/adb/meZram 
+done &
+
+set --
+
+set "ro.lmk.low" "ro.lmk.critical_upgrade" "ro.lmk.upgrade_pressure" "ro.lmk.kill_heaviest_task" "ro.lmk.kill_timeout_ms" "ro.lmk.psi_complete_stall_ms" "ro.lmk.thrashing_limit_decay" "persist.device_config.lmkd_native.thrashing_limit_critical" "mezram_test"
+
+tl="ro.lmk.thrashing_limit"
+
+resetprop lmkd.reinit 1
+
+for i in $(seq 2); do
+	rm_prop "$@"
+	if [ "$(resetprop ro.miui.ui.version.code)" ]; then
+		# rm_prop $tl
+	fi
+	resetprop lmkd.reinit 1
+	sleep 2m
+done &
