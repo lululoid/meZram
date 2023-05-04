ec22a89e4873c06c2e235a8b6f618845ff3f93ad
lululuid <nreiwn@gmail.com>
2023-05-04 08:35:09 +0700

    update prompts

--- customize.sh
+++ customize.sh
@@ -17,7 +17,7 @@ lmkd_apply() {
     # determine if device is lowram?
     logger "totalmem = $totalmem"
     if [ "$totalmem" -lt 2097152 ]; then
-		ui_print "- Device is low ram. Applying low raw tweaks"
+		ui_print "⚠️ Device is low ram. Applying low raw tweaks"
 		mv "$MODPATH"/system.props/low-ram-system.prop "$MODPATH"/system.prop
     else
 		mv "$MODPATH"/system.props/high-performance-system.prop "$MODPATH"/system.prop
@@ -40,8 +40,8 @@ lmkd_apply() {
 		resetprop $(echo "$prop" | sed s/=/' '/)
 	done
 
-    resetprop lmkd.reinit 1 && ui_print "- lmkd reinitialized"
-    ui_print "- lmkd multitasking tweak applied."
+    resetprop lmkd.reinit 1 && ui_print "> lmkd reinitialized"
+    ui_print "> lmkd multitasking tweak applied."
     ui_print "  Give the better of your RAM."
     ui_print "  RAM better being filled with something"
     ui_print "  useful than left unused"
@@ -53,9 +53,9 @@ count_SWAP() {
     swap_size=$((totalmem / 2))
     count=0
 
-    ui_print "- Please SELECT SWAP SIZE"
-	ui_print "  Press VOLUME UP to SELECT"
-    ui_print "  Press VOLUME DOWN to CONTINUE"
+    ui_print "> Please select SWAP size"
+	ui_print "  Press VOLUME - to SELECT"
+	ui_print "  Press VOLUME + to DEFAULT"
     ui_print "  DEFAULT is $((totalmem/1024/2))MB of SWAP"
     
     while true; do
@@ -69,7 +69,7 @@ count_SWAP() {
 				ui_print "  $count. 50% of RAM ($((swap_size/1024))MB SWAP) --> RECOMMENDED"
 			elif [ $count -eq 2 ]; then
 				count=$((count + 1))
-				ui_print "  $count. NO SWAP"
+				ui_print "  $count. No SWAP"
 				swap_size=0
 			elif [ $swap_in_gb -lt $totalmem_gb ]; then
 				count=$((count + 1))
@@ -96,7 +96,7 @@ make_swap(){
     dd if=/dev/zero of="$2" bs=1024 count="$1" > /dev/null                   
     mkswap "$2" > /dev/null
     swapon "$2" > /dev/null
-    ui_print "- SWAP is RUNNING"
+    ui_print "- SWAP is running"
 }
 
 mount /data > /dev/null
@@ -108,7 +108,7 @@ free_space=$(df /data -P | sed -n '2p' | sed 's/[^0-9 ]*//g' | sed ':a;N;$!ba;s/
 logger "$(df /data -P | sed -n '2p' | sed 's/[^0-9 ]*//g' | sed ':a;N;$!ba;s/\n/ /g')"
 
 if [ -d "/data/adb/modules/meZram" ]; then
-    ui_print "- Thank you so much 😊."
+    ui_print "> Thank you so much 😊."
     ui_print "  You've installed this module before"
 fi 
 
@@ -123,19 +123,19 @@ if [ ! -f $swap_filename ]; then
 		ui_print "  $((free_space/1024))MB available. $((swap_size/1024))MB needed"
 		make_swap "$swap_size" $swap_filename
     elif [ -z "$free_space" ]; then
-		ui_print "- Make sure you have $((swap_size / 1024))MB space available data partition"
+		ui_print "> Make sure you have $((swap_size / 1024))MB space available data partition"
 		ui_print "  Make SWAP?"
-		ui_print "  Press VOLUME DOWN (YES) or VOLUME UP (NO)"
+		ui_print "  Press VOLUME - (YES) or VOLUME + (NO)"
 
 		while true; do
         	timeout 0.5 /system/bin/getevent -lqc 1 2>&1 > "$TMPDIR"/events &
         	sleep 0.1
 			if (grep -q 'KEY_VOLUMEDOWN *DOWN' "$TMPDIR"/events); then
-				ui_print "- Starting making SWAP. Please wait a moment"; sleep 0.5
+				ui_print "> Starting making SWAP. Please wait a moment"; sleep 0.5
 				make_swap "$swap_size" $swap_filename
 				break
 			elif (grep -q 'KEY_VOLUMEUP *DOWN' "$TMPDIR"/events); then
-				cancelled=$(ui_print "- Making SWAP cancelled by user")
+				cancelled=$(ui_print "> Not making SWAP")
 				$cancelled && logger "$cancelled"
             	break
 			fi
@@ -143,12 +143,12 @@ if [ ! -f $swap_filename ]; then
     elif [ $count -eq 3 ]; then
 		true
     else
-		ui_print "- Storage full. Please free up your storage"
+		ui_print "> Storage full. Please free up your storage"
     fi 
 fi
 
 if [ "$sdk_level" -lt 28 ]; then
-    ui_print "- Your android version is not supported. Performance tweaks won't applied."
+    ui_print "> Your android version is not supported. Performance tweaks won't applied."
     ui_print "  Please upgrade your phone to Android 9+"
 else
     lmkd_apply
--- service.sh
+++ service.sh
@@ -1,78 +1,82 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
+LOGDIR=/data/adb/meZram
 
 # Calculate memory to use for zram
 NRDEVICES=$(grep -c ^processor /proc/cpuinfo | sed 's/^0$/1/')
 totalmem=$(free | grep -e "^Mem:" | sed -e 's/^Mem: *//' -e 's/  *.*//')
-zram_size=$(((totalmem / 2) * 1024))
+zram_size=$((totalmem * 1024 / 2))
 lmkd_pid=$(getprop init.svc_debug_pid.lmkd)
 
 logger(){
-    true && echo "$*" >> "$MODDIR"/meZram.log
+	td=$(date +%R:%S:%N)
+    true && echo "$td $*" >> "$MODDIR"/meZram.log
 }
 
-logger "zram_size = $zram_size"
+rm -rf "$MODDIR"/lmkd.log "$MODDIR"/meZram.log
+
 logger "NRDEVICES = $NRDEVICES"
 logger "totalmem = $totalmem"
+logger "zram_size = $zram_size"
 logger "lmkd_pid = $lmkd_pid"
 
 for zram0 in /dev/block/zram0 /dev/zram0; do
     if [ -n "$(ls $zram0)" ]; then
-		swapoff $zram0 2> "$MODDIR"/meZram.log
-		echo 1 > /sys/block/zram0/reset 2>> "$MODDIR"/meZram.log 
+		swapoff $zram0 && logger "$zram0 turned off"
+		echo 1 > /sys/block/zram0/reset
+		logger "$zram0 reset"
 
 		# Set up zram size, then turn on both zram and swap
-		echo $zram_size > /sys/block/zram0/disksize 2>> "$MODDIR""/meZram."log 
+		echo "$zram_size" > /sys/block/zram0/disksize
+		logger "set $zram0 disksize to $zram_size"
 
 		# Set up maxium cpu streams
-		echo "$NRDEVICES" > /sys/block/zram0/max_comp_streams 2>> "$MODDIR"/meZram.log 
-		mkswap $zram0 2>> "$MODDIR"/meZram.log 
-		swapon $zram0 2>> "$MODDIR"/meZram.log 
-		echo "$zram0 succesfully activated" > "$MODDIR"/meZram.log 
+		logger "making $zram0 and set max_comp_streams=$NRDEVICES"
+		echo "$NRDEVICES" > /sys/block/zram0/max_comp_streams
+		mkswap $zram0
+		swapon $zram0
     else
-		echo "$zram0 not exist in this device" >> "$MODDIR"/meZram.log 
+		logger "$zram0 not exist in this device" 
     fi
 done
 
-swapon /data/swap_file >> "$MODDIR"/meZram.log 
+swapon /data/swap_file && logger "swap is turned on"
 
 # echo '1' > /sys/kernel/tracing/events/psi/enable 2>> "$MODDIR"/meZram.log 
 
-if [ ! -d /data/adb/meZram ]; then
-	logger "- Trying to make folder"
-	mkdir -p /data/adb/meZram 2>> "$MODDIR"/meZram.log
-fi
-
 # peaceful logger
 while true; do
-    logcat --pid "$lmkd_pid" -f "$MODDIR"/lmkd.log
-    sleep 1m 
-	cp "$MODDIR"/*.log /data/adb/meZram
-	if [ "$(du lmkd.log|awk '{printf $1}')" -gt 10240 ]; then
-		break
-	fi
+	logcat --pid "$lmkd_pid" -f "$MODDIR"/lmkd.log
+	break
 done &
 
 rm_prop(){                                
 	for prop in "$@"; do
-		[ "$(resetprop "$prop")" ] && resetprop --delete "$prop" && logger "$prop deleted" >> "$MODDIR"/meZram.log
+		resetprop "$prop" && resetprop --delete "$prop" && logger "$prop deleted"
 	done
 }
 
 # set "ro.config.low_ram" "ro.lmk.use_psi" "ro.lmk.use_minfree_levels" "ro.lmk.low" "ro.lmk.medium" "ro.lmk.critical" "ro.lmk.critical_upgrade" "ro.lmk.upgrade_pressure" "ro.lmk.downgrade_pressure" "ro.lmk.kill_heaviest_task" "ro.lmk.kill_timeout_ms" "ro.lmk.psi_partial_stall_ms" "ro.lmk.psi_complete_stall_ms" "ro.lmk.thrashing_limit" "ro.lmk.thrashing_limit_decay" "ro.lmk.swap_util_max" "ro.lmk.swap_free_low_percentage" "ro.lmk.debug" "sys.lmk.minfree_levels"
 
 set --
-set "ro.lmk.low" "ro.lmk.upgrade_pressure" "ro.lmk.kill_heaviest_task" "ro.lmk.kill_timeout_ms" "ro.lmk.psi_complete_stall_ms" "ro.lmk.critical_upgrade" "ro.lmk.psi_partial_stall_ms" "ro.lmk.thrashing_limit_decay" "ro.lmk.swap_free_low_percentage"
+set "ro.lmk.low" "ro.lmk.upgrade_pressure" "ro.lmk.kill_heaviest_task" "ro.lmk.kill_timeout_ms" "ro.lmk.psi_complete_stall_ms" "ro.lmk.psi_partial_stall_ms" "ro.lmk.thrashing_limit_decay" "ro.lmk.swap_free_low_percentage" "sys.lmk.minfree_levels" "ro.lmk.downgrade_pressure" "ro.lmk.medium" "ro.lmk.critical" 
 
 tl="ro.lmk.thrashing_limit"
 
 resetprop lmkd.reinit 1
 
 for i in $(seq 2); do
-	rm_prop "$@"
-	if [ "$(resetprop ro.miui.ui.version.code)" ]; then
-		rm_prop $tl
-	fi
-	resetprop lmkd.reinit 1
+    rm_prop "$@"
+    if resetprop ro.miui.ui.version.code >/dev/null 2>&1; then
+        rm_prop $tl
+    fi
+    resetprop lmkd.reinit 1
+    sleep 2m
+done &
+
+mkdir -p "$LOGDIR"
+
+while true; do
+	cp -u "$MODDIR"/*.log "$LOGDIR"
 	sleep 2m
 done &
