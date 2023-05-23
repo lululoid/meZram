72b9ce696c9b77545c3b4f7dea0b06df72ad5e79
NAME <mail@gmail.com>
2023-05-24 06:23:31 +0700

    update json

--- wrapit.sh
+++ wrapit.sh
@@ -13,12 +13,12 @@ if [ -z "$versionCode" ]; then
     versionCode=$((versionCode + 1))
 fi
 
-sed -i "s/version=v[0-9.]*-beta/version=v$version-beta/g; s/versionCode=[0-9]*/versionCode=$versionCode/g" module.prop
-sed -i "s/\"version\": \"[^\"]*\"/\"version\": \"$version-beta\"/" meZram.json
+sed -i "s/version=v[0-9.]*-beta-psi/version=v$version-beta-psi/g; s/versionCode=[0-9]*/versionCode=$versionCode/g" module.prop
+sed -i "s/\"version\": \"[^\"]*\"/\"version\": \"$version-beta-psi\"/" meZram.json
 sed -i "s/\"versionCode\": \"[^\"]*\"/\"versionCode\": \"$versionCode\"/" meZram.json
-sed -i "s#\"zipUrl\": \"https://github.com/lululoid/meZram/releases/download/v[0-9.]*-beta/meZram-v[0-9.]*_[0-9]*-beta.zip\",#\"zipUrl\": \"https://github.com/lululoid/meZram/releases/download/v$version-beta/meZram-v$version\_$versionCode-beta.zip\",#g; s#\"changelog\": \"https://github.com/lululoid/meZram/releases/download/v[0-9.]*-beta/meZram-v[0-9.]*_[0-9]*-beta-changelog.md\"#\"changelog\": \"https://github.com/lululoid/meZram/releases/download/v$version-beta/meZram-v$version\_$versionCode-beta-changelog.md\"#g" meZram.json
+sed -i "s#\"zipUrl\": \"https://github.com/lululoid/meZram/releases/download/v[0-9.]*-beta-psi/meZram-v[0-9.]*_[0-9]*-beta-psi.zip\",#\"zipUrl\": \"https://github.com/lululoid/meZram/releases/download/v$version-beta-psi/meZram-v$version\_$versionCode-beta-psi.zip\",#g; s#\"changelog\": \"https://github.com/lululoid/meZram/releases/download/v[0-9.]*-beta-psi/meZram-v[0-9.]*_[0-9]*-beta-psi-changelog.md\"#\"changelog\": \"https://github.com/lululoid/meZram/releases/download/v$version-beta-psi/meZram-v$version\_$versionCode-beta-psi-changelog.md\"#g" meZram.json
 
-changelog_file=$(ls | grep -o 'meZram-v[0-9]\+\.[0-9]\+_[0-9]\+-beta-changelog\.md')
-mv "$changelog_file" "meZram-v${version}_$versionCode-beta-changelog.md"
+changelog_file=$(ls | grep -o 'meZram-v[0-9]\+\.[0-9]\+_[0-9]\+-beta-psi-changelog\.md')
+mv "$changelog_file" "meZram-v${version}_$versionCode-beta-psi-changelog.md"
 
-7za a "meZram-v${version}_$versionCode-beta.zip" $zipfiles
+7za a "meZram-v${version}_$versionCode-beta-psi.zip" $zipfiles
