abafceff5bebefd884497f4b7f29ee4996d10fd9
lululoid <ereiwn@gmail.com>
2023-09-08 22:05:48 +0700

    fix: update link json

--- wrapit.sh
+++ wrapit.sh
@@ -36,11 +36,10 @@ sed -i "s/\"versionCode\": \"[^\"]*\"/\"versionCode\": \"$versionCode\"/" meZram
 sed -i "s#\"zipUrl\": \"https://github.com/lululoid/meZram/releases/download/v[0-9.]*_[0-9]*-beta-psi/meZram-v[0-9.]*_[0-9]*-beta-psi.zip\",#\"zipUrl\": \"https://github.com/lululoid/meZram/releases/download/v$version\_$versionCode-beta-psi/meZram-v$version\_$versionCode-beta-psi.zip\",#g; s#\"changelog\": \"https://github.com/lululoid/meZram/releases/download/v[0-9.]*_[0-9]*-beta-psi/meZram-v[0-9.]*_[0-9]*-beta-psi-changelog.md\"#\"changelog\": \"https://github.com/lululoid/meZram/releases/download/v$version\_$versionCode-beta-psi/meZram-v$version\_$versionCode-beta-psi-changelog.md\"#g" meZram.json
 
 module_name=$(sed -n 's/id=\(.*\)/\1/p' module.prop)
-mv "$changelog_file" "$module_name-v${version}_$versionCode-changelog.md"
 changelog_file=$(find . -type f -iname "*changelog.md")
 
-mv "$changelog_file" "$module_name-v${version}_$versionCode-changelog.md"
-7za a "packages/$module_name-v${version}_$versionCode.zip" . \
+mv "$changelog_file" "$module_name-v${version}_$versionCode-beta-psi-changelog.md"
+7za a "packages/$module_name-v${version}_$versionCode-beta-psi.zip" . \
 	-x!meZram.json \
 	-x!meZram*changelog.md \
 	-x!wrapit.sh \
@@ -49,4 +48,4 @@ mv "$changelog_file" "$module_name-v${version}_$versionCode-changelog.md"
 	-x!.git \
 	-x!pic \
 	-x!tmp \
-  -x!test*
+	-x!test*
