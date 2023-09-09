b0f96e421ee33dcce47e2529657bbcb5c3a55d29
lululoid <ereiwn@gmail.com>
2023-09-09 07:37:56 +0700

    fix: wrapit.sh

--- wrapit.sh
+++ wrapit.sh
@@ -30,13 +30,12 @@ if [ -z "$versionCode" ]; then
 fi
 
 # U think I lazy to type? No, i just really forgetful sometimes
-sed -i "s/version=v[0-9.]*-beta-psi/version=v$version-beta-psi/g; s/versionCode=[0-9]*/versionCode=$versionCode/g" module.prop
-sed -i "s/\"version\": \"[^\"]*\"/\"version\": \"$version-beta-psi\"/" meZram.json
+sed -i "s/version=v[0-9.]*/version=v$version/g; s/versionCode=[0-9]*/versionCode=$versionCode/g" module.prop
+sed -i "s/\"version\": \"[^\"]*\"/\"version\": \"$version\"/" meZram.json
 sed -i "s/\"versionCode\": \"[^\"]*\"/\"versionCode\": \"$versionCode\"/" meZram.json
-sed -i "s#\"zipUrl\": \"https://github.com/lululoid/meZram/releases/download/v[0-9.]*_[0-9]*-beta-psi/meZram-v[0-9.]*_[0-9]*-beta-psi.zip\",#\"zipUrl\": \"https://github.com/lululoid/meZram/releases/download/v$version\_$versionCode-beta-psi/meZram-v$version\_$versionCode-beta-psi.zip\",#g; s#\"changelog\": \"https://github.com/lululoid/meZram/releases/download/v[0-9.]*_[0-9]*-beta-psi/meZram-v[0-9.]*_[0-9]*-beta-psi-changelog.md\"#\"changelog\": \"https://github.com/lululoid/meZram/releases/download/v$version\_$versionCode-beta-psi/meZram-v$version\_$versionCode-beta-psi-changelog.md\"#g" meZram.json
+sed -i "s#\"zipUrl\": \"https://github.com/lululoid/meZram/releases/download/v[0-9.]*_[0-9]*/meZram-v[0-9.]*_[0-9]*.zip\",#\"zipUrl\": \"https://github.com/lululoid/meZram/releases/download/v$version\_$versionCode/meZram-v$version\_$versionCode.zip\",#g; s#\"changelog\": \"https://github.com/lululoid/meZram/releases/download/v[0-9.]*_[0-9]*/meZram-v[0-9.]*_[0-9]*-changelog.md\"#\"changelog\": \"https://github.com/lululoid/meZram/releases/download/v$version\_$versionCode/meZram-v$version\_$versionCode-changelog.md\"#g" meZram.json
 
 module_name=$(sed -n 's/id=\(.*\)/\1/p' module.prop)
-mv "$changelog_file" "$module_name-v${version}_$versionCode-changelog.md"
 changelog_file=$(find . -type f -iname "*changelog.md")
 
 mv "$changelog_file" "$module_name-v${version}_$versionCode-changelog.md"
